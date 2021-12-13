65fa775a98acb7b45272f4d0f84ea49782c732b8
Gema R Volek <gemarubycastro@gmail.com>
2021-12-13 15:32:32 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by: Gema Volek and is open-sourced on
-        <a
-          href="https://github.com/gvolek/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by: Gema Volek and is open-sourced on
+          <a
+            href="https://github.com/gvolek/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
