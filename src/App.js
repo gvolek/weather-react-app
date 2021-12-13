49a1b5e7bdcc44ed0ed76f886671ebdf81f2836f
Gema R Volek <gemarubycastro@gmail.com>
2021-12-13 14:57:34 -0700

    Created project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by: Gema Volek and is open-sourced on GitHub
+        <a href="" target="_blank"></a>
+      </footer>
     </div>
   );
 }
-
-export default App;
