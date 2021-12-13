189fdc503fafab20521ed9f78ed2d2a2179ae6c7
Gema R Volek <gemarubycastro@gmail.com>
2021-12-13 16:12:32 -0700

    created rough draft of weather app & fake data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
+import "./Weather.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by: Gema Volek and is open-sourced on
           <a
