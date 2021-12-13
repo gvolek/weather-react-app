2e8007f842e6e447560c1f3f81c3d6ac78537e23
Gema R Volek <gemarubycastro@gmail.com>
2021-12-13 15:11:32 -0700

    added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by: Gema Volek and is open-sourced on
-        <a href="https://github.com/gvolek/weather-react-app" target="_blank">
+        <a
+          href="https://github.com/gvolek/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           {" "}
           GitHub
         </a>
